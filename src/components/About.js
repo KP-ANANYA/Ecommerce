a7d00086e237bccadae934b56e03d5af7d1f15ca
KP-ANANYA <ananyakp90@gmail.com>
2024-05-31 12:09:19 +0530

    first push

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <center>
+      
+      <p>Welcome to our website! We are dedicated to providing you with the best experience.</p>
+      <p>Feel free to explore and learn more about what we offer.</p>
+      <h1>thank you bye bye!!!!</h1>
+      .
+      </center>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import banner from "../components/banner.jpg"
+
+function Home() {
+  return (
+    <div>
+      <h1><center>Welcome to KP`s Website</center></h1>
+<img src={banner} width={1500}></img>
+      <h2></h2>
+    </div>
+  );
+}
+
+export default Home;
